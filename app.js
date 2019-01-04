68ecff4645baca376acb978f834f560d5dcdb16b
patrik76nilsson <43703228+patrik76nilsson@users.noreply.github.com>
2019-01-04 11:04:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,8 @@ function create(env, ctx) {
         "/bgclock.html":"bgclock.html",
         "/report":"reportindex.html",
         "/translations":"translationsindex.html",
-        "/clock.html":"clock.html"
+        "/clock.html":"clock.html",
+	"/hud.html":"hud.html"
     };
 
 	Object.keys(appPages).forEach(function(page) {
